fca6db59d868dbe0e1a5028afbecb6a94fc0128b
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2022-11-10 03:50:22 +0300

    tests have written for money calc

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -2,9 +2,12 @@
 
 namespace Mscakir\MscBinance\Tests;
 
+use Money\Currency;
+use Money\Money;
 use Mscakir\MscBinance\Clients\BinanceClient;
 use Mscakir\MscBinance\MscBinanceServiceProvider;
 use Mscakir\MscBinance\Objects\BinanceClientOptions;
+use Mscakir\MscBinance\Objects\Models\BinanceBalance;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Tests\CreatesApplication;
 
@@ -35,6 +38,27 @@ class UnitTest extends BaseTestCase
         $apiClient = new BinanceClient("Binance", BinanceClientOptions::default());
 
         $this->assertTrue("Binance" == $apiClient->name);
-        $this->assertTrue();
+    }
+
+    public function test_total_is_sum_free_and_locked()
+    {
+        $binanceBalance = new BinanceBalance();
+        $currency = new Currency("XBT");
+        $binanceBalance->asset = "BTC";
+        $binanceBalance->free = new Money("1000.00", $currency);
+        $binanceBalance->locked = new Money("2300.00", $currency);
+
+        $this->assertEquals(Money::sum($binanceBalance->free, $binanceBalance->locked), $binanceBalance->getTotal());
+    }
+
+    public function test_total_is_sum_manual_free_and_locked()
+    {
+        $binanceBalance = new BinanceBalance();
+        $currency = new Currency("XBT");
+        $binanceBalance->asset = "BTC";
+        $binanceBalance->free = new Money("1000.00", $currency);
+        $binanceBalance->locked = new Money("2300.00", $currency);
+
+        $this->assertEquals(3300, $binanceBalance->getTotal()->getAmount());
     }
 }
